8519097032b6b73807888baf4b57139ead7a8d26
trevize77 <maciej.spychalski@gmail.com>
2020-08-05 00:09:47 +0200

    Initial commit

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  const apiRoot = 'https://damp-bayou-82531.herokuapp.com/v1/task/';
   const trelloApiRoot = 'https://damp-bayou-82531.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');

99f8c2da05a83048abba033bbae8e7ee52c5b785
trevize77 <maciej.spychalski@gmail.com>
2020-08-06 21:11:06 +0200

    Initial commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'https://calm-stream-84867.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'https://calm-stream-84867.herokuapp.com/v1/trello/';
+  const apiRoot = 'https://tranquil-bastion-06649.herokuapp.com/v1/task/';
+  const trelloApiRoot = 'https://tranquil-bastion-06649.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 

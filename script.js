a59850719e67f7cf382c5345705a9aad74d1d79a
trevize77 <maciej.spychalski@gmail.com>
2020-11-08 23:09:33 +0100

    Task32.4

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'http://localhost:8080/v1/';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  const apiRoot = 'https://agile-retreat-71843.herokuapp.com/v1/';
+  const trelloApiRoot = 'https://agile-retreat-71843.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
